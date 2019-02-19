76b3e6da7cbbaaaefc3a3e176ca8a32568375f35
Hendrik Geßner <HENGE01@gmail.com>
2019-02-19 16:56:08 +0100

    Added setup.py to enable package installing

--- setup.py
+++ setup.py
@@ -3,21 +3,19 @@ import setuptools
 long_description = open("README.md").read()
 
 setuptools.setup(
-    name="minima-test-zbyszek",
-    version="0.0.1",
-    author="Zbigniew Jędrzejewski-Szmek",
-    author_email="zbyszek@in.waw.pl",
-    description="A library to find function maxima",
-
+    name="maxima-tlange",
+    version="0.0.2",
+    author="Theresa Lange",
+    author_email="tlange@math.tu-berlin.de",
+    description="Find local maxima",
     long_description=long_description,
     long_description_content_type="text/markdown",
-
-    url="https://github.com/potsdam-python-workshop/testing",
+    url="https://github.com/ItsNotYou/testing",
     packages=['maxima'],
-
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    tests_require=['numpy', 'pytest'],
 )
